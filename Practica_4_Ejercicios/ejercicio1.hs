31cce8b740956698c5713d7f6353a3a3e088f112
Franco <frncxyz@protonmail.com>
2024-04-21 21:37:08 -0300

    Ejercicios hechos con notas de consulta

--- Practica_4_Ejercicios/ejercicio1.hs
+++ Practica_4_Ejercicios/ejercicio1.hs
@@ -1,8 +1,8 @@
 
-fibonacci:: Int -> Int
+fibonacci:: Integer -> Integer
 
-fibonacci n | n == 0 = 0
-            | n == 1 = 1
-            | otherwise = (fibonacci(n-1) + fibonacci(n-2))
+fibonacci n | n==0 = 0
+            | n==1 = 1
+            | otherwise = fibonacci(n-1) + fibonacci(n-2)
 
 -- Ver práctica

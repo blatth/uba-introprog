85fa45e6d3f7c5deb47e1effb205ed689546b6bc
Franco <frncxyz@protonmail.com>
2024-04-25 10:37:44 -0300

    Resolucion del simulacro de parcial

--- Simulacro.hs
+++ Simulacro.hs
@@ -1,3 +1,51 @@
+{-
+ENUNCIADO
+ 
+    Ejercicio 1
+    Para empezar a diseñar la novedosa y rupturista red social Y el famoso Elio Mark nos ha pedido que desarrollemos algunas funciones básicas, que tendrán como objetido representar algunas relaciones e interacciones entre los usuarios. Para esto nos envió las siguientes especificaciones en lenguaje semiformal y nos pidió que hagamos el desarrollo enteramente en Haskell, utilizando los tipos requeridos y solamente las funciones que se ven en Introducción a la Programación de Exactas-UBA.
+ 
+    problema relacionesValidas (relaciones: seq⟨String x String⟩) : Bool {
+      requiere: {True}
+      asegura: {(res = true) <=> relaciones no contiene ni tuplas repetidas1, ni tuplas con ambas componentes iguales}
+    }
+    1 A los fines de este problema consideraremos que dos tuplas son iguales si el par de elementos que las componen (sin importar el orden) son iguales.
+ 
+    problema personas (relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
+      requiere: {relacionesValidas(relaciones)}
+      asegura: {res no tiene elementos repetidos}
+      asegura: {res tiene exactamente los elementos que figuran en alguna tupla de relaciones, en cualquiera de sus posiciones}
+    }
+ 
+    problema amigosDe (persona: String, relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
+      requiere: {relacionesValidas(relaciones)}
+      asegura: {res tiene exactamente los elementos que figuran en las tuplas de relaciones en las que una de sus componentes es persona}
+    }
+ 
+    problema personaConMasAmigos (relaciones: seq⟨String x String⟩) : String {
+      requiere: {relaciones no vacía}
+      requiere: {relacionesValidas(relaciones)}
+      asegura: {res es el Strings que aparece más veces en las tuplas de relaciones (o alguno de ellos si hay empate)}
+    }
+ 
+LISTA DE FUNCIONES PERMITIDAS
+=============================
+mod :: Integral a => a -> a -> a
+div :: Integral a => a -> a -> a
+fst :: (a, b) -> a
+snd :: (a, b) -> b
+sqrt :: Floating a => a -> a
+(:) :: a -> [a] -> [a]
+(++) :: [a] -> [a] -> [a]
+head :: [a] -> a
+tail :: [a] -> [a]
+fromIntegral :: (Integral a, Num b) => a -> b
+fromInteger :: Num a => Integer -> a
+arim´eticas (+, -, *, /)
+potencia (**, ^)
+not :: Bool -> Bool
+logicas (&&, ||, ==, /=, >, <, >=, <=)
+-}
+
 module Simulacro where
 
 -- 1

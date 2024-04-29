ee3fa0bc79d61fe91225ece7176573a5f93f0db3
Blatth <francoadrianmoran@gmail.com>
2024-04-29 11:37:48 -0300

    Parciales movidos

---
+++ Parciales/Simulacro.hs
@@ -0,0 +1,100 @@
+{-
+ENUNCIADO
+ 
+    Ejercicio 1
+    Para empezar a diseñar la novedosa y rupturista red social Y el famoso Elio Mark nos ha pedido que desarrollemos algunas funciones básicas, que tendrán como objetido representar algunas relaciones e interacciones entre los usuarios. Para esto nos envió las siguientes especificaciones en lenguaje semiformal y nos pidió que hagamos el desarrollo enteramente en Haskell, utilizando los tipos requeridos y solamente las funciones que se ven en Introducción a la Programación de Exactas-UBA.
+ 
+    problema relacionesValidas (relaciones: seq⟨String x String⟩) : Bool {
+      requiere: {True}
+      asegura: {(res = true) <=> relaciones no contiene ni tuplas repetidas1, ni tuplas con ambas componentes iguales}
+    }
+    1 A los fines de este problema consideraremos que dos tuplas son iguales si el par de elementos que las componen (sin importar el orden) son iguales.
+ 
+    problema personas (relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
+      requiere: {relacionesValidas(relaciones)}
+      asegura: {res no tiene elementos repetidos}
+      asegura: {res tiene exactamente los elementos que figuran en alguna tupla de relaciones, en cualquiera de sus posiciones}
+    }
+ 
+    problema amigosDe (persona: String, relaciones: seq⟨String x String⟩) : seq⟨String⟩ {
+      requiere: {relacionesValidas(relaciones)}
+      asegura: {res tiene exactamente los elementos que figuran en las tuplas de relaciones en las que una de sus componentes es persona}
+    }
+ 
+    problema personaConMasAmigos (relaciones: seq⟨String x String⟩) : String {
+      requiere: {relaciones no vacía}
+      requiere: {relacionesValidas(relaciones)}
+      asegura: {res es el Strings que aparece más veces en las tuplas de relaciones (o alguno de ellos si hay empate)}
+    }
+ 
+LISTA DE FUNCIONES PERMITIDAS
+=============================
+mod :: Integral a => a -> a -> a
+div :: Integral a => a -> a -> a
+fst :: (a, b) -> a
+snd :: (a, b) -> b
+sqrt :: Floating a => a -> a
+(:) :: a -> [a] -> [a]
+(++) :: [a] -> [a] -> [a]
+head :: [a] -> a
+tail :: [a] -> [a]
+fromIntegral :: (Integral a, Num b) => a -> b
+fromInteger :: Num a => Integer -> a
+arim´eticas (+, -, *, /)
+potencia (**, ^)
+not :: Bool -> Bool
+logicas (&&, ||, ==, /=, >, <, >=, <=)
+-}
+
+module Simulacro where
+
+-- 1
+
+relacionesValidas:: [(String, String)] -> Bool
+relacionesValidas [] = True
+relacionesValidas [x, y]   | x /= y = True
+relacionesValidas (x:y:xs) | fst(x) == snd(x) || x == y = False
+                           | otherwise = relacionesValidas(y:xs)
+
+-- 2
+
+personas:: [(String, String)] -> [String]
+personas [] = []
+personas ((p1, p2):ps) = (sacarRepetido p1 (sacarRepetido p2 (personas ps)))
+
+{- nombré p1=persona1 y ps2=persona2 para no confundirme sobre qué evalúa cada función -}
+
+sacarRepetido:: Eq t => t -> [t] -> [t]
+sacarRepetido x [] = [x]
+sacarRepetido x (y:ys) | x==y = y:ys
+                       | otherwise = y:sacarRepetido x ys 
+
+{- 
+si x/=y => devuelve la lista con y para luego aplicar la función (de forma recursiva) al término que dejé y el resto de la lista con la cual ""concatené"" 
+-}
+
+{-
+Esto era todo un paso engorroso que no tenía sentido, puede reducirse todo a una línea (sacarRepetido)
+pertenece:: (Eq String) => String -> [String] -> Bool
+
+pertenece _ [] = []
+pertenece x (y:ys) = x == y || pertenece x ys
+
+estaRepetido:: (Eq String) => [String] -> Bool
+estaRepetido [] = False
+estaRepetido (x:xs) = pertenece x xs || estaRepetido xs
+
+quitar:: (Eq String) => String -> [String] -> [String]
+quitar _ [] = []
+quitar z (x:xs) | z == x = xs
+                | otherwise = x:quitar z xs
+-}
+
+-- 3
+
+amigosDe:: String -> [(String, String)] -> [String]
+amigosDe _ [] = []
+amigosDe x ((p1, p2):ys) | x == p1 = p2:amigosDe x ys
+                         | x == p2 = p1:amigosDe x ys
+                         | otherwise = amigosDe x ys
+
---
+++ Parciales/parcial20232c.hs
@@ -0,0 +1,52 @@
+{- consigna: https://github.com/blatth/introprog/blob/master/primerparcial20232c.pdf -}
+
+-- 1
+
+computoVotosAfirmativos:: [Int] -> Int
+computoVotosAfirmativos [] = 0
+computoVotosAfirmativos (x:xs) = x + computoVotosAfirmativos xs
+
+{- dada una lista con números, los suma. si la lista es vacía o ya verificó todos los valores, va al caso base de []=0 -}
+
+votosEnBlanco:: [(String, String)] -> [Int] -> Int -> Int
+votosEnBlanco _ votosAfirmativos votosTotales = votosTotales - (computoVotosAfirmativos votosAfirmativos)
+
+{- dado lo siguiente: [("Biden","Biondini")] [1,1,2,3,4,1,1,] 180, donde 1: fórmula, 2: votosafirmativos, 3: votostotales, le resta a la totalidad de los votos el cómputo de votos afirmativos realizado sobre la lista que le es dada. sería como sacar un cardinal por complemento. -}
+
+
+-- 2
+
+{-
+formulasValidas:: [(String, String)] -> Bool
+formulasValidas [] = True
+formulasValidas ((a,b):xs) | a == b = False
+                           | 
+                           | 
+                           | otherwise = formulasValidas xs 
+-} 
+
+{- consultar cómo hacer para poner que cuando candidatoPerteneceTupla = true, formulaValida = false  -}
+
+candidatoPerteneceTupla:: String -> [(String, String)] -> Bool
+
+candidatoPerteneceTupla n [] = False
+candidatoPerteneceTupla n ((a,b):xs) | n == a = True
+                                     | n == b = True 
+                                     | otherwise = candidatoPerteneceTupla n xs
+
+{- candidatoPerteneceTupla verifica que el candidato dado esté o no en la tupla. si se compara con una lista (que no es la vacía) y da false, comienza el proceso de vuelta. si se compara con un vacío termina la secuencia (caso base). -}
+
+-- 3
+
+division:: Int -> Int -> Float
+division a b = fromIntegral a / fromIntegral b
+
+numeroDeVotos:: String -> [(String, String)] -> [Int] -> Int
+numeroDeVotos presidente ((candidato, vice):xs) (votos:ys) | presidente == candidato = computoVotosAfirmativos(votos:ys)
+                                                           | otherwise = numeroDeVotos presidente xs ys
+
+porcentajeDeVotos:: String -> [(String, String)] -> [Int] -> Float
+porcentajeDeVotos presidente formula (votos:ys) = division ((numeroDeVotos presidente formula (votos:ys))*100) (computoVotosAfirmativos (votos:ys))
+
+{- Me da siempre 100, obviamente, porque siempre le paso la misma lista y compara con esa. Siento como si faltase un input (votosTotales), pero no sé otra forma de interpretar el enunciado. Preguntar -}
+

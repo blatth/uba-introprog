94f6538473d80be58081ed267b61b7f5029d026c
Franco <frncxyz@protonmail.com>
2024-05-29 14:45:55 -0300

    Moví el ej 5.2

--- Practicas/Practica_7_Ejercicios/ej2.py
+++ Practicas/Practica_7_Ejercicios/ej2.py
@@ -32,20 +32,3 @@ def borrarVocales(palabra:str)->str:
         if (not(es_vocal(palabra[i]))):
             salida+=palabra[i]
     return salida
-
-
-#Ej 5.2
-
-def pertenece2(s:list[int], e:int) -> bool:
-    res:bool = False
-    for v in s:
-        if (v==e):
-            return v
-
-def pertenece3(s:list[int], e:int) -> bool:
-    return e in s
-
-def pertenece_a_cada_uno_2(s:list[list[int]], e:int, res:list[bool]):
-    res.clear()
-    for s in range:
-        res.append (pertenece3 (lista, e))
